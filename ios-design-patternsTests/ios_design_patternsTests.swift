9027047c10728d8076d68e7c98a6d4813bfc5819
Astemir Eleev <astemireleev@gmail.com>
2018-04-22 14:27:10 +0300

    Initial Commit

---
+++ ios-design-patternsTests/ios_design_patternsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_design_patternsTests.swift
+//  ios-design-patternsTests
+//
+//  Created by Astemir Eleev on 22/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import XCTest
+@testable import ios_design_patterns
+
+class ios_design_patternsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
