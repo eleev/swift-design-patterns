120c322d576833d7d52583b5631237b9dd0df369
Astemir Eleev <astemireleev@gmail.com>
2018-04-22 14:30:06 +0300

    Implemented Singleton design patter

---
+++ ios-design-patterns/Singleton/Singleton.swift
@@ -0,0 +1,22 @@
+//
+//  Singleton.swift
+//  ios-design-patterns
+//
+//  Created by Astemir Eleev on 22/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+class Singleton {
+    
+    // MARK: - Singleton property
+    
+    let instnage = Singleton()
+    
+    // MARK: - Private initializer
+    
+    private init() {
+        // Private initialier ensires that there will no access to from the outside, so instances can only be created inside the class
+    }
+}
