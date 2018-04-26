817eb5e67f901b85bb7cbe0f3f817e75fecf1a2c
Astemir Eleev <astemireleev@gmail.com>
2018-04-26 16:42:26 +0300

    Implemented Burger struct

---
+++ ios-design-patterns/Builder/BurgerBuilder.swift
@@ -0,0 +1,20 @@
+//
+//  BurgerBuilder.swift
+//  ios-design-patterns
+//
+//  Created by Astemir Eleev on 25/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol BurgerBuilder {
+    
+    var name: String { get }
+    var patties: Int { get }
+    var bacon: Bool { get }
+    var cheese: Bool { get }
+    var pickles: Bool { get }
+    var mustard: Bool { get }
+    var tomato: Bool { get }
+}
