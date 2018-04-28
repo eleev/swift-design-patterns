0ca2cf36892db5e53c95491a3a3c6a9687566787
Astemir Eleev <astemireleev@gmail.com>
2018-04-28 20:26:01 +0300

    Updated project scheme

--- ios-design-patterns/AppDelegate.swift
+++ ios-design-patterns/AppDelegate.swift
@@ -13,9 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch.        
         return true
     }
 
