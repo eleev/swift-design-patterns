5c157b653c0760767d1d225b56355155f744d458
Astemir Eleev <astemireleev@gmail.com>
2018-09-08 19:46:09 +0300

    Finished code sample

--- Common Design Patterns/Behavioral/NullObject/NullObject.playground/Contents.swift
+++ Common Design Patterns/Behavioral/NullObject/NullObject.playground/Contents.swift
@@ -1,13 +1,66 @@
 
 import UIKit
 
-struct Product {
-    var name: String
-    var calories: Int
-    var price: NSNumber
+protocol Product {
+    var name: String { get }
+    var calories: Int { get }
+    var price: NSNumber { get }
 }
 
-extension Product: CustomStringConvertible {
+struct Apple: Product {
+    var name: String = "Apple"
+    var calories: Int = 50
+    var price: NSNumber = 3
+    
+    init() { }
+}
+
+struct Banana: Product {
+    var name: String = "Banana"
+    var calories: Int = 85
+    var price: NSNumber = 5
+    
+    init() { }
+}
+
+struct Juice: Product {
+    var name: String = "Orange Juice"
+    var calories: Int = 150
+    var price: NSNumber = 20
+    
+    init() { }
+}
+
+struct NullObjectProduct: Product {
+    var name: String = "Void"
+    var calories: Int = 0
+    var price: NSNumber = 0
+    
+    init() { }
+}
+
+extension Apple: CustomStringConvertible {
+    
+    var description: String {
+        return "name: \(name), calories: \(calories), price: \(price)"
+    }
+}
+
+extension Banana: CustomStringConvertible {
+    
+    var description: String {
+        return "name: \(name), calories: \(calories), price: \(price)"
+    }
+}
+
+extension Juice: CustomStringConvertible {
+    
+    var description: String {
+        return "name: \(name), calories: \(calories), price: \(price)"
+    }
+}
+
+extension NullObjectProduct: CustomStringConvertible {
     
     var description: String {
         return "name: \(name), calories: \(calories), price: \(price)"
@@ -15,26 +68,21 @@ extension Product: CustomStringConvertible {
 }
 
 class Basket {
-    
-    private var products: [Product]
-    
-    subscript(index: Int) -> Product? {
+
+    subscript(index: Int) -> Product {
         get {
             guard index > -1, index < products.count else {
-                return nil
+                return NullObjectProduct()
             }
             return products[index]
         }
     }
     
-    init() {
-        products = []
-    }
+    private var products: [Product] = []
     
     func add(product: Product) {
         products += [product]
     }
-    
 }
 
 extension Basket: CustomStringConvertible {
@@ -52,36 +100,39 @@ class MarketViewController: UIViewController {
     
     var basket = Basket()
     
+    // MARK: - Initializers
+    
     init() {
-        let banana = Product(name: "Banana", calories: 85, price: 5)
-        let apple = Product(name: "Apple", calories: 50, price: 3)
-        let juice = Product(name: "Orange Juice", calories: 150, price: 20)
+//        let banana = Product(name: "Banana", calories: 85, price: 5)
+//        let apple = Product(name: "Apple", calories: 50, price: 3)
+//        let juice = Product(name: "Orange Juice", calories: 150, price: 20)
         
-        basket.add(product: banana)
-        basket.add(product: apple)
-        basket.add(product: juice)
+//        basket.add(product: banana)
+//        basket.add(product: apple)
+//        basket.add(product: juice)
+//
+//        print(basket)
+//
+//        if let thirdProduct = basket[3] {
+//            print("Third product: ", thirdProduct)
+//        } else {
+//            print("nil")
+//        }
+
+        basket.add(product: Banana())
+        basket.add(product: Apple())
+        basket.add(product: Juice())
         
         print(basket)
         
-        if let thirdProduct = basket[2] {
-            print("Third product: ", thirdProduct)
-        } else {
-            print("nil")
-        }
+        let thirdProduct = basket[3]
+        print(thirdProduct)
         
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - Lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Prepare the view based on the data source
     }
 }
 
